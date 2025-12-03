ce11d4643008fcfd0cda4ffa8bc5072b6d53cae3
Ge0m <geldi.omeri@gmail.com>
2025-12-02 22:37:21 -0600

    submit tester

--- apps/analyzer/src/main.jsx
+++ apps/analyzer/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import UploadWidgetLauncher from './UploadWidgetLauncher';
+import UploadWidgetLauncher from './UploadWidgetLauncher.jsx';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
